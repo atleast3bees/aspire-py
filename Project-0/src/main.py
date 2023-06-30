c2005c244991be72abf1173827882fe550153ab8
Rori Wu <atleast3bees@gmail.com>
2023-06-30 16:30:51 -0400

    Project 0 Complete

--- Project-0/src/main.py
+++ Project-0/src/main.py
@@ -1,1 +1,18 @@
 print("hello world")
+
+'''
+ ________________________________________ 
+/ I never failed to convince an audience \
+| that the best thing they could do was  |
+\ to go away.                            /
+ ---------------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+'''
+
+#-rw-r--r--  1 wurm1  staff   2.2K Jun 30 15:50 README.md
+#drwxr-xr-x  3 wurm1  staff    96B Jun 30 16:26 src
+
