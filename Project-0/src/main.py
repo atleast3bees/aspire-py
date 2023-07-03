c5b08477d9c285462140fa71b65f8a225eee8699
Rori Wu <atleast3bees@gmail.com>
2023-07-03 11:28:35 -0400

    Project-0

---
+++ Project-0/src/main.py
@@ -0,0 +1,34 @@
+print('hello world')
+
+#-rw-rw-r--@ 1 wurm1  staff   2.2K Jun 24 10:05 README.md
+#drwxr-xr-x@ 4 wurm1  staff   128B Jun 30 13:12 src
+
+# _________________________________ 
+#/ QOTD:                           \
+#|                                 |
+#| I'm not a nerd -- I'm "socially |
+#\ challenged".                    /
+# --------------------------------- 
+#        \   ^__^
+#         \  (oo)\_______
+#            (__)\       )\/\
+#                ||----w |
+#                ||     ||
+print('hello world')
+
+#-rw-rw-r--@ 1 wurm1  staff   2.2K Jun 24 10:05 README.md
+#drwxr-xr-x@ 4 wurm1  staff   128B Jun 30 13:12 src
+
+'''
+ _________________________________ 
+/ QOTD:                           \
+|                                 |
+| I'm not a nerd -- I'm "socially |
+\ challenged".                    /
+ --------------------------------- 
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+'''
